2e333a6377a03b9385cf381d0d98fbe3b36d61e7
krdpk1717 <62544105+krdpk1717@users.noreply.github.com>
2020-05-20 05:03:19 +0530

    User provides config file as input and then test query starts
    Test-case
    With correct config, test case succeeds and pop-up comes for the same
    With incorrect config, test case fails and error pop-up comes

--- fetcher_query_intf.py
+++ fetcher_query_intf.py
@@ -1,11 +1,7 @@
 from neo4jintf.docker.features import fetcher_query
 import os
 import pdb
-def test_query_insert(filepath):
-    print("filepath is {}".format(filepath))
-    print("DLLLLLL1") 
-    os.environ["ENV_FILEPATH"]=filepath
+def test_query_insert():
     status = fetcher_query.test_query_insert()
-    print("DLLLLLL1_")
     return status
     
--- main.py
+++ main.py
@@ -1,48 +1,33 @@
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.factory import Factory
 from kivy.properties import ObjectProperty
-from kivy.uix.widget import Widget
-import fetcher_query_intf as fetcher_query
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 
-class MyGrid1(Widget):
-    cred_file = ObjectProperty(None)
-    resp = None
-
-    def bt(self):
-        if self.cred_file.text == "":
-            print("found empty")
-            empty_input()
-            self.reset()
-        else:
-            print("DKKKK3")
-            MyGrid1.resp = fetcher_query.test_query_insert(self.cred_file.text)
-            print("DKKKK3_")
-            if not MyGrid1.resp:
-                auth_error()
-                self.reset()
-
-            else:
-                print(MyGrid1.resp)
-                self.reset()
-                show_popup()
-
-    def reset(self):
-        self.cred_file.text = ""
+import os
 
 
-class P():
-    pass
+from fetcher_query_intf import fetcher_query
 
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+# class SaveDialog(FloatLayout):
+#     save = ObjectProperty(None)
+#     text_input = ObjectProperty(None)
+#     cancel = ObjectProperty(None)
 
 def auth_error():
     content = Button(text='Close')
-    popup = Popup(title="Authentication Failed", content=content, auto_dismiss=False,
+    popup = Popup(title="Operation Failed", content=content, auto_dismiss=False,
                   size_hint=(None, None), size=(300, 200))
     popup.open()
 
     content.bind(on_press=popup.dismiss)
-
 
 def show_popup():
     content = Button(text='Close')
@@ -52,30 +37,63 @@ def show_popup():
 
     content.bind(on_press=popup.dismiss)
 
+class Root(FloatLayout):
+    loadfile = ObjectProperty(None)
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    target_file_path = 'neo4jintf/docker/features/config/.env'
 
-def error_popup():
-    content = Button(text='Error Reading File')
-    popup = Popup(title="Re Try", content=content, auto_dismiss=False,
-                  size_hint=(None, None), size=(300, 200))
-    popup.open()
+    def dismiss_popup(self):
+        self._popup.dismiss()
 
-    content.bind(on_press=popup.dismiss)
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def show_save(self):
+        with open(Root.target_file_path, 'w') as stream:
+            stream.write(self.text_input.text)
+        self.text_input.text = ''
+        result = fetcher_query.test_query_insert()
+        print("result is {}".format(result))
+
+        if not result:
+            auth_error()
+
+        else:
+            print(result)
+            show_popup()
+        # content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        # self._popup = Popup(title="Save file", content=content,
+        #                     size_hint=(0.9, 0.9))
+        # self._popup.open()
+
+    def load(self, path, filename):
+        print("Input file path is {} and name is {}".format(path, filename))
+        #Root.target_file_path = filename[0] + '.out'
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+
+        self.dismiss_popup()
+
+    def save(self, path, filename):
+        # print("DKKKK file path is {}".format(filename))
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+
+        self.dismiss_popup()
 
 
-def empty_input():
-    content = Button(text='Re-Try')
-    popup = Popup(title="Found Missing Path", content=content,
-                  auto_dismiss=False,
-                  size_hint=(None, None), size=(300, 200))
-    popup.open()
-
-    content.bind(on_press=popup.dismiss)
+class Editor(App):
+    pass
 
 
-class MyApp(App):
-    def build(self):
-        return MyGrid1()
+Factory.register('Root', cls=Root)
+Factory.register('LoadDialog', cls=LoadDialog)
+#Factory.register('SaveDialog', cls=SaveDialog)
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    Editor().run()
